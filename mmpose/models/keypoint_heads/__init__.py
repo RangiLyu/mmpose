884361f2adf1bc70018c29d9affd8f9d837b9ff7
jinsheng <jinsheng@sensetime.com>
2021-05-26 21:24:19 +0800

    update heads

--- mmpose/models/keypoint_heads/__init__.py
+++ mmpose/models/keypoint_heads/__init__.py
@@ -3,13 +3,12 @@ from .bottom_up_simple_head import BottomUpSimpleHead
 from .fc_head import FcHead
 from .interhand_3d_head import Interhand3DHead
 from .paf_head import PAFHead
-from .paf_simple_head import PAFSimpleHead
 from .temporal_regression_head import TemporalRegressionHead
 from .top_down_multi_stage_head import TopDownMSMUHead, TopDownMultiStageHead
 from .top_down_simple_head import TopDownSimpleHead
 
 __all__ = [
     'TopDownSimpleHead', 'TopDownMultiStageHead', 'TopDownMSMUHead', 'PAFHead',
-    'PAFSimpleHead', 'BottomUpHigherResolutionHead', 'BottomUpSimpleHead',
-    'FcHead', 'TemporalRegressionHead', 'Interhand3DHead'
+    'BottomUpHigherResolutionHead', 'BottomUpSimpleHead', 'FcHead',
+    'TemporalRegressionHead', 'Interhand3DHead'
 ]
