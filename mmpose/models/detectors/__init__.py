78afc11f9f67a289cff29950502878ec1b5462d3
jinsheng <jinsheng@sensetime.com>
2021-05-26 14:41:16 +0800

    rename to PartAffinityField

--- mmpose/models/detectors/__init__.py
+++ mmpose/models/detectors/__init__.py
@@ -2,10 +2,12 @@ from .bottom_up import BottomUp
 from .interhand_3d import Interhand3D
 from .mesh import ParametricMesh
 from .multi_task import MultiTask
+from .paf import PartAffinityField
 from .pose_lifter import PoseLifter
 from .top_down import TopDown
 
 __all__ = [
     'TopDown', 'BottomUp', 'ParametricMesh', 'MultiTask', 'PoseLifter',
-    'Interhand3D'
+    'Interhand3D',
+    'PartAffinityField'
 ]
